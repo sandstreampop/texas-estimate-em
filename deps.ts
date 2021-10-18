c18c04d786ab457e883084ae046784012b54300e
Viktor S. Romild <viktor.sandstrom@animech.com>
2021-10-18 23:43:43 +0200

    fix: try even more

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 // @deno-types="https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts"
-import React from "https://jspm.dev/react@17.0.2";
+import React, { createElement as h } from "https://jspm.dev/react@17.0.2";
 // @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from "https://jspm.dev/react-dom@17.0.2/server";
 // @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/react-dom.d.ts"
@@ -7,4 +7,4 @@ import ReactDOM from "https://jspm.dev/react-dom@17.0.2";
 
 import { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 
-export { Application, Router, React, ReactDOM, ReactDOMServer };
+export { Application, Router, React, ReactDOM, ReactDOMServer, h };
